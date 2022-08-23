b26dcd88c788a04fd9a3e101b2ce544e73e5ed23
Paschal O <pogbuagu84@gmail.com>
2022-08-23 22:00:17 +0000

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,10 @@ deploy python apps
 deploy in dev env
 deployment5 master
 deployment4
+<<<<<<< HEAD
 deploy to staging
 echo good job.
 tested and accepted
+=======
+deploy to staging 
+>>>>>>> 4b45608 (deployment to the staging evnironment)
