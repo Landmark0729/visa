8ae27772212a9226cad3da0688f7bb6a7efefaf4
Paschal O <pogbuagu84@gmail.com>
2022-08-23 22:00:17 +0000

    conflict 2 resolved

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,7 @@ deploy python apps
 deploy in dev env
 deployment5 master
 deployment4
-<<<<<<< HEAD
-deploy to staging
+deploy to staging env
 echo good job.
-tested and accepted
-=======
-deploy to staging 
->>>>>>> 4b45608 (deployment to the staging evnironment)
+tested and accepted 
+
